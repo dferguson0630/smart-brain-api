5f911c297a9b5fec216f43874f15393a1b3c94dd
dferguson0630 <47204218+kanyetehgiant@users.noreply.github.com>
2020-02-11 16:34:07 -0500

    smart brain server 1.0.0

---
+++ Controllers/Image.js
@@ -0,0 +1,26 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '535456f5d083408685b311056061930e'
+});
+
+
+const handleApiCall = (req, res) => {
+	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => response.json(data)).catch(err => res.status(400).json('unable to work with API'))
+
+}
+
+
+//Function to call the id stored in the database, increment the number of submissions(entries) for that user by one, and return the number of entries
+const handleImage = (req, res) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id).increment('entries',1)
+		.returning('entries').then(entries => res.json(entries[0]))
+		.catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./Controllers/Register');
+const signin = require('./Controllers/signIn');
+const profile = require('./Controllers/Profile');
+const image = require('./Controllers/Image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'umami17',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+//ROOT
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+
+//SIGNIN
+//Uses dependency injection to pass bcrypt to signIn.js
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
+
+//REGISTER
+//Uses dependency injection to pass bcrypt and db to Register.js
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+//PROFILE
+//Uses dependency injection to pass db to Profile.js
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+
+//IMAGE
+//Uses dependency injection to pass db to Image.js
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+
+app.listen(3001, () => {
+	console.log('app is running on port 3001')
+});
