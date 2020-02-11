f3b718b52945163053cc6f7169e1c4e6c96d335c
dferguson0630 <47204218+kanyetehgiant@users.noreply.github.com>
2020-02-11 18:46:16 -0500

    environmental variable call

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 
 app.listen((process.env.PORT || 3001), () => {
-	console.log(`app is running on port ${PORT}`)
+	console.log(`app is running on port ${process.env.PORT}`)
 });
