f4ae8a8060138ff78b1b7b7b803d3430a16f1c08
dferguson0630 <47204218+kanyetehgiant@users.noreply.github.com>
2020-02-12 11:03:21 -0500

    handler response issue

--- Controllers/Image.js
+++ Controllers/Image.js
@@ -7,13 +7,14 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
 	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-		.then(data => res.json(data)).catch(err => res.status(400).json('unable to work with API'))
+		.then(data => res.json(data))
+		.catch(err => res.status(400).json('unable to work with API'))
 
 }
 
 
 //Function to call the id stored in the database, increment the number of submissions(entries) for that user by one, and return the number of entries
-const handleImage = (req, res) => {
+const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id).increment('entries',1)
 		.returning('entries').then(entries => res.json(entries[0]))
